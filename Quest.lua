cc62f769efc0f105a9b2daae5c9c8809f19b4709
Reynaldimatialo <121653267+Reynaldimatialo@users.noreply.github.com>
2023-11-25 19:51:16 +0800

    Create Quest.lua

---
+++ Quest.lua
@@ -0,0 +1,8 @@
+local function onBossDefeated()
+  -- Put your code here to reward the player for defeating the boss
+  local player = game.PlayersService:GetPlayerFromCharacter(script.Parent.Parent)
+  player:AddCash(986000000)
+  player:AddExperience(500000000)
+end
+
+script.Parent.BossDefeatedEvent:Connect(onBossDefeated)
